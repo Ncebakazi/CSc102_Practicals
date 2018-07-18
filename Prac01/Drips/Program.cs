fa6e4d919aa6995c49d12cace5742808b78152fa
ncebakazi <g17g2767@campus.ru.ac.za>
2018-07-18 15:43:35 +0200

    Prac1

---
+++ Prac01/Drips/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How often do you hear a drip drop (in seconds)?");
+            string num =Console.ReadLine();
+            int number = Convert.ToInt32(num);
+            
+
+            
+            double hour = 3600/number * 0.00025;
+            double day = 86400/number * 0.00025;
+            double week = 604800/number * 0.00025;
+
+            Console.WriteLine("This wastes " + hour + " litres of water every hour");
+            Console.WriteLine("This wastes " + day + " litres of water every day");
+            Console.WriteLine("This wastes " + week + " litres of water every week");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Prac01/WholeGlasses/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WholeGlasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How often do you hear a drip drop (in seconds)?");
+            string num = Console.ReadLine();
+            double number = Convert.ToDouble(num);
+
+            double day = 86400 / number * 0.00025;
+
+         
+            double ans = day / 0.25;
+            Console.WriteLine(Math.Floor(ans) + " full glasses of water are being wasted" );
+            Console.ReadLine();
+
+
+        }
+    }
+}
